88ae5eb811bff8bc1bb01b974a95638461707977
Martin Tomasi <martin.tomasi@gmail.com>
2018-04-23 00:18:46 +0200

    fixed some routing issues

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,39 @@
 extern crate actix;
 extern crate actix_web;
-extern crate rusoto_core as aws;
-extern crate rusoto_s3 as s3;
-extern crate serde;
-extern crate toml;
-#[macro_use]
-extern crate serde_derive;
-#[macro_use]
-extern crate log;
 extern crate bytes;
 extern crate failure;
 extern crate futures;
+#[macro_use]
+extern crate log;
 extern crate log4rs;
+extern crate rusoto_core as aws;
+extern crate rusoto_s3 as s3;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
 
 use std::sync::Arc;
 
-use actix_web::{http::{ContentEncoding, StatusCode},
-                server,
-                App,
-                Body,
-                HttpMessage,
-                HttpRequest,
-                HttpResponse};
+use actix_web::{server, App, Body, HttpMessage, HttpRequest, HttpResponse,
+                http::{ContentEncoding, StatusCode}};
 use failure::Error;
-use futures::{future, Future, Stream};
+use futures::{Future, Stream, future::Either};
+
+trait OptionExt<T> {
+    fn filter_val<P: FnOnce(&T) -> bool>(self, predicate: P) -> Self;
+}
+
+impl<T> OptionExt<T> for Option<T> {
+    fn filter_val<P: FnOnce(&T) -> bool>(self, predicate: P) -> Self {
+        if let Some(x) = self {
+            if predicate(&x) {
+                return Some(x);
+            }
+        }
+        None
+    }
+}
 
 type Result<T> = std::result::Result<T, Error>;
 
@@ -72,37 +82,52 @@ struct State {
     config: Config,
 }
 
-struct PathInfo {
-    bucket: Option<String>,
-    key: String,
-}
+fn handle_response(res: s3::GetObjectOutput) -> HttpResponse {
+    use bytes::Bytes;
+    debug!("S3 response: {:?}", res);
 
-fn parse_path<S: AsRef<str>>(path: &str, bucket: Option<S>) -> Option<PathInfo> {
-    let parts: Vec<_> = path.splitn(2, '/').collect();
-    match parts.len() {
-        1 => Some(PathInfo {
-            bucket: bucket.map(|s| s.as_ref().to_string()),
-            key: path.into(),
-        }),
-        2 => Some(PathInfo {
-            bucket: Some(parts[0].into()),
-            key: parts[1].into(),
-        }),
-        _ => unreachable!(),
+    let body = res.body
+        .expect("No body for response")
+        .map(Bytes::from)
+        .map_err(Error::from);
+    let mut builder = HttpResponse::Ok();
+
+    if let Some(content_length) = res.content_length {
+        debug!("Content-Length: {}", content_length);
+        builder.content_length(content_length as u64);
     }
-}
+    if let Some(content_type) = res.content_type {
+        if content_type.starts_with("audio") || content_type.starts_with("video")
+            || content_type.starts_with("image")
+        {
+            builder.content_encoding(ContentEncoding::Identity);
+        }
+        debug!("Content-Type: {}", content_type);
+        builder.content_type(content_type.as_str());
+    }
+    if let Some(e_tag) = res.e_tag {
+        builder.header("ETag", e_tag);
+    }
+    if let Some(content_range) = res.content_range {
+        debug!("Content-Range: {}", content_range);
+        builder.header("Content-Range", content_range);
+        builder.status(StatusCode::PARTIAL_CONTENT);
+    }
+    if let Some(accept_ranges) = res.accept_ranges {
+        debug!("Accept-Ranges: {}", accept_ranges);
+        builder.header("Accept-Ranges", accept_ranges);
+    }
+    if let Some(last_modified) = res.last_modified {
+        builder.header("Last-Modified", last_modified);
+    }
 
-fn four_oh_four() -> Box<Future<Item = HttpResponse, Error = Error>> {
-    Box::new(future::ok(
-        HttpResponse::NotFound().body("Resource not found!"),
-    ))
+    debug!("--- Sending request --- ");
+    builder.body(Body::Streaming(Box::new(body.map_err(From::from))))
 }
 
 fn handler(req: HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    use bytes::Bytes;
     use s3::S3;
 
-    // TODO handle missing keys (404)
     // TODO reject empty keys
     let client = Arc::clone(&req.state().s3_client);
     let config = &req.state().config;
@@ -111,20 +136,12 @@ fn handler(req: HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = E
         .and_then(|r| r.to_str().ok())
         .map(From::from);
 
-    let path: String = req.match_info().query("path").unwrap();
-
-    let PathInfo { bucket, key } = match parse_path(&path, config.bucket.as_ref()) {
-        Some(info) => {
-            if info.bucket.is_none() {
-                return four_oh_four();
-            } else {
-                info
-            }
-        }
-        None => {
-            return four_oh_four();
-        }
-    };
+    let key: String = req.match_info().query("path").unwrap();
+    let bucket = req.match_info()
+        .query("bucket")
+        .ok()
+        .filter_val(|s: &String| s.len() > 0)
+        .or(config.bucket.clone());
 
     debug!("Request headers: {:?}", req.headers());
     let resp = client
@@ -134,63 +151,49 @@ fn handler(req: HttpRequest<State>) -> Box<Future<Item = HttpResponse, Error = E
             range,
             ..Default::default()
         })
+        .then(|result| {
+            if let Err(s3::GetObjectError::NoSuchKey(_)) = result {
+                Ok(Either::B(HttpResponse::NotFound().body("404 - Not found")))
+            } else {
+                result.map(Either::A)
+            }
+        })
         .from_err()
-        .map(|res| {
-            debug!("S3 response: {:?}", res);
-
-            let body = res.body
-                .expect("No body for response")
-                .map(Bytes::from)
-                .map_err(Error::from);
-            let mut builder = HttpResponse::Ok();
-
-            if let Some(content_length) = res.content_length {
-                debug!("Content-Length: {}", content_length);
-                builder.content_length(content_length as u64);
-            }
-            if let Some(content_type) = res.content_type {
-                if content_type.starts_with("audio") || content_type.starts_with("video")
-                    || content_type.starts_with("image")
-                {
-                    builder.content_encoding(ContentEncoding::Identity);
-                }
-                debug!("Content-Type: {}", content_type);
-                builder.content_type(content_type.as_str());
-            }
-            if let Some(e_tag) = res.e_tag {
-                builder.header("ETag", e_tag);
-            }
-            if let Some(content_range) = res.content_range {
-                debug!("Content-Range: {}", content_range);
-                builder.header("Content-Range", content_range);
-                builder.status(StatusCode::PARTIAL_CONTENT);
-            }
-            if let Some(accept_ranges) = res.accept_ranges {
-                debug!("Accept-Ranges: {}", accept_ranges);
-                builder.header("Accept-Ranges", accept_ranges);
-            }
-            if let Some(last_modified) = res.last_modified {
-                builder.header("Last-Modified", last_modified);
-            }
-
-            debug!("--- Sending request --- ");
-            builder.body(Body::Streaming(Box::new(body.map_err(From::from))))
+        .map(|res| match res {
+            Either::A(res) => handle_response(res),
+            Either::B(res) => res,
         });
 
     Box::new(resp)
 }
 
+fn build_route(config: &Config) -> String {
+    if config.url_prefix.is_empty() {
+        if config.bucket.is_some() {
+            String::from("/{path:.+}")
+        } else {
+            String::from("/{{bucket}}/{{path:.+}}")
+        }
+    } else {
+        if config.bucket.is_some() {
+            format!("/{}/{{bucket}}/{{path:.+}}", config.url_prefix)
+        } else {
+            format!("/{}/{{path:.+}}", config.url_prefix)
+        }
+    }
+}
+
 fn run() -> Result<()> {
     configure_logger();
     let config = read_config()?;
+    if let Some(ref bucket) = config.bucket {
+        info!("Hosting content from bucket '{}' ", bucket);
+    }
     let region = config.region.parse()?;
     let s3_client = Arc::new(s3::S3Client::simple(region));
     let addr = format!("{}:{}", config.host, config.port);
-    let route = if config.url_prefix.is_empty() {
-        String::from("/{path:.+}")
-    } else {
-        format!("/{}/{{path:.+}}", config.url_prefix)
-    };
+    let route = build_route(&config);
+    info!("Main route mounted at {}", route);
     server::new(move || {
         App::with_state(State {
             s3_client: Arc::clone(&s3_client),
@@ -205,21 +208,5 @@ fn run() -> Result<()> {
 fn main() {
     if let Err(e) = run() {
         eprintln!("Error: {}", e);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn parse_path() {
-        use super::parse_path;
-
-        const BUCKET: &str = "bucket_name";
-        const KEY: &str = "key/to/the/file.mp4";
-
-        let path = format!("{}/{}", BUCKET, KEY);
-        let result = parse_path::<String>(&path, None).unwrap();
-        assert_eq!(result.bucket.unwrap(), BUCKET);
-        assert_eq!(result.key, KEY);
     }
 }
